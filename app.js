7eaf44d2bbd08ee325a048fe5c81f3f270f359e6
ramani-3528 <ramaneshwari3528@gmail.com>
2025-06-09 21:05:39 +0530

    add

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 function calculate(){
     let number1 = parseFloat(document.getElementById("num1").value);
     let number2 = parseFloat(document.getElementById("num2").value);
-    let operator= document.getElementById("calc").value;
+    let operator= document.getElementById("cal").value;
     let result;
-    if(operator=="addition"){
-        result = number1+number2;
-    }
-    else if(operator=="subtraction"){
-        result = number1-number2;
-    }
-    else if(operator=="multiplication"){
-        result = number1*number2;
-    }
-    else if(operator=="division"){
-        result = number1/number2;
-    }
-    else{
-        result = "Invalid operator";
+    switch (operator){
+        case "addition":
+            result = number1+number2;
+            break;
+        case "subtraction":
+            result = number1-number2;
+            break;
+        case "multipilication":
+            result = number1*number2;
+            break;
+        case "division":
+            result = number1/number2;
+            break;
+        default:
+            result = "Invalid Operator"
     }
     document.getElementById("result").value = result;
 }
